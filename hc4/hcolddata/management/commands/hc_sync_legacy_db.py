12e03ee13585b8c0a387e30389f9d605bd162bfa
Kestutis Januskevicius <infohata@gmail.com>
2019-04-25 19:49:46 +0300

    matching countries manually by name or flag/code2;

--- hc4/hcolddata/management/commands/hc_sync_legacy_db.py
+++ hc4/hcolddata/management/commands/hc_sync_legacy_db.py
@@ -49,16 +49,40 @@ class Command(BaseCommand):
         self.stdout.write(' Countries in the old database: {}'.format(str(oc.count())))
         # fc is the found countries list. This list will be the indication of "in sync" countries
         fc = {}
-        # nc is the not found countries list.
-        nc = {}
-        for o in nc:
-            # f is the city currently being synced
+        # nfnc/nfoc are  not found new/old countries lists.
+        nfnc = []
+        nfoc = []
+        # mcm is a manual oc.flag to nc.code2 match
+        mcm = {
+            'bx': 'BN', 'ck': 'CC', 'ez': 'CZ', 'iv': 'CI', 'fm': 'FM', 're': 'RE', 'rs': 'RU', 'us': 'US',
+            'vt': 'VA', 'vm': 'VN', 'cg': 'CD', 'cf': 'CG', 'tt': 'TL', 'pt': 'PS', 'sv': 'SJ', 'pc': 'PN',
+            'fs': 'TF', 'fk': 'FK', 
+        }
+
+        for o in oc:
             for n in nc:
                 if o.name == n.name:
-                    self.stdout.write('  Found {}'.format(o.name))
-                    fc.update({o.name: n})
-                    oc
-                
-
-
+                    # self.stdout.write('  Found {}'.format(o.name))
+                    fc.update({n.id: o.id})
+                    break
+            else:
+                if o.flag in mcm:
+                    try:
+                        n = nc.get(code2=mcm[o.flag])
+                    except:
+                        self.stderr.write('  {} not found in new DB'.format(o.name))
+                    else:
+                        fc.update({n.id: o.id})
+                else:
+                    nfoc.append(o)
+        for n in nc:
+            if n.id not in fc:
+                nfnc.append(n)
+        self.stdout.write('  Countries matched: {}'.format(str(len(fc))))
+        self.stdout.write('  Countries not matched in new DB: {}'.format(str(len(nfnc))))
+        for nfn in nfnc:
+            self.stdout.write('    {}, {}: {}'.format(str(nfn.id), str(nfn.code2), str(nfn.name)))
+        self.stdout.write('  Countries not matched in old DB: {}'.format(str(len(nfoc))))
+        for nfo in nfoc:
+            self.stdout.write('    {}, {}: {}'.format(str(nfo.id), str(nfo.flag), str(nfo.name)))
 
